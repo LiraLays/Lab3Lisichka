8c54e9cebfbdb4ee9993a9c8f1f78bce7c3abbed
Andrey <awentus2528@bk.ru>
2025-11-06 03:18:45 +0300

    Реализованы первые 3 пункта в библиотеке
    Реализованы первые 3 пункта в библиотеке. Добавлена простейшая реализация интерфейса для примера

---
+++ WpfLab3/ClassLab3/Class1.cs
@@ -0,0 +1,256 @@
+﻿using System.Diagnostics;
+using static ClassLab3.Class1;
+
+namespace ClassLab3
+{
+    public class Class1
+    {
+        // Перечисление для классификации типа теста для TryMakeStep
+        public enum TaskType
+        {
+            Sum,
+            Count,
+            Max,
+            None
+        }
+
+        public struct Cycle
+        {
+            public int i;
+            public int res;
+            public int[] array;
+            public TaskType taskType;
+            public int T;
+        }
+
+        public static Cycle InitializeCycle(int[] arr, TaskType checkedTask, int actualT)
+        {
+            Cycle cycle = new Cycle();
+            cycle.i = 0;
+            cycle.array = arr;
+            cycle.taskType = checkedTask;
+            cycle.res = checkedTask == TaskType.Max ? int.MinValue : 0;
+            cycle.T = actualT;
+            return cycle;
+        }
+
+        /// <summary>
+        /// Метод для генерации случайного массива из n элементов
+        /// </summary>
+        /// <param name="n">Количество элементов</param>
+        /// <param name="minVal">Минимальное значение элемента</param>
+        /// <param name="maxVal">Максимальное значение элемента</param>
+        /// <returns>Массив элементов</returns>
+        public static int[] GenerateNSizeArray(int n, int minVal, int maxVal)
+        {
+            Random random = new Random();
+
+            int[] newArray = new int[n];
+
+            for (int i = 0; i < n; i++)
+                newArray[i] = random.Next(minVal, maxVal);
+
+            return newArray;
+        }
+
+        /// <summary>
+        /// Метод, для расчёта суммы массива
+        /// </summary>
+        /// <param name="array">Массив целых чисел</param>
+        /// <returns>Возвращает значения post и суммы массива в формате Tuple</returns>
+        public static (bool, int) PrefixSum(int[] array)
+        {
+            bool post = true; // Выполнение постусловия
+            int res = 0; // Результат
+            int j = array.Length; // Длина масива
+            int i = 0;
+
+            // Проверка инварианта на верность перед первой итерацией
+            Debug.Assert(res == 0);
+
+            while (i < j)
+            { 
+                // Инвариант перед шагом: res = сумма первых i элементов
+                // Тело
+               
+                res += array[i];
+                i++;
+
+                // Инвариант после шага: res = сумма первых i элементов
+            }
+
+            // Выход i == j => res = сумма всех элементов
+            return (post, res); 
+        }
+
+        ///// <summary>
+        ///// Метод, для расчёта суммы массива
+        ///// </summary>
+        ///// <param name="array">Массив целых чисел</param>
+        ///// <returns>Возвращает значения post и суммы массива в формате Tuple</returns>
+        //public static (bool, int) PrefixSumBySteps(int[] array)
+        //{
+        //    bool post = true; // Выполнение постусловия
+        //    int res = 0; // Результат
+        //    int j = array.Length; // Длина масива
+        //    int i = 0;
+
+        //    // Проверка инварианта на верность перед первой итерацией
+        //    Debug.Assert(res == 0);
+
+        //    bool result = TryMakeStep(i, j, res, array, TaskType.Sum);
+
+        //    // Выход i == j => res = сумма всех элементов
+        //    return (post, res);
+        //}
+
+        public static (bool, int) MakeOperation(int[] array, TaskType taskType, int T = 0)
+        {
+            bool post = true; // Выполнение постусловия
+            int res = taskType == TaskType.Max ? int.MinValue : 0; // Результат
+            int j = array.Length; // Длина масива
+            int i = 0;
+
+            // Проверка инварианта на верность перед первой итерацией
+            Debug.Assert(res == (taskType == TaskType.Max ? int.MinValue : 0));
+
+            while (i < j)
+            {
+                // Инвариант перед шагом: res = сумма первых i элементов
+                // Тело
+
+                switch (taskType)
+                {
+                    case TaskType.Sum:
+                        res = IncreaseSum(res, i, array); break;
+                    case TaskType.Count:
+                        res = IncreaseCount(res, i, array, T); break;
+                    case TaskType.Max:
+                        res = ChangeMax(res, i, array); break;
+                }
+
+                i++;
+
+                // Инвариант после шага: res = сумма первых i элементов
+            }
+
+            // Выход i == j => res = сумма всех элементов
+            return (post, res);
+        }
+
+        /// <summary>
+        /// Метод, для выполнения шага
+        /// </summary>
+        /// <param name="i">Итератор</param>
+        /// <param name="j">Длина массива</param>
+        /// <param name="res">Результат</param>
+        /// <param name="array">Массив значений</param>
+        /// <param name="taskType">Тип операции</param>
+        /// <param name="T">Значение для сравнения</param>
+        /// <returns>Возвращает значение итератора, максимальное значение, результат на данном шаге
+        ///          В случае если цикл закончился возвращает все значения null
+        /// </returns>
+        public static (int?, int?, int?) MakeStep(int i, int res, int[] array, TaskType taskType, int T = 0)
+        {
+            int j = array.Length;
+            (bool result, int resInt) = TryMakeStep(i, j, res, array, taskType, T);
+            if (result)
+                return (i, j, resInt);
+            else
+                return (null, null, null);
+        }
+    
+        /// <summary>
+        /// Попытка сделать ход. Проверяет не вышло ли значение i за границы j.
+        /// </summary>
+        /// <param name="i">Итератор</param>
+        /// <param name="j">Длина массива</param>
+        /// <param name="res">Результат</param>
+        /// <param name="array">Массив значений</param>
+        /// <param name="taskType">Тип операции</param>
+        /// <param name="T">Значение для сравнения</param>
+        /// <returns>true - если изменение прошло успешно, false - в случае окончания цикла</returns>
+        public static (bool, int) TryMakeStep(int i, int j, int res, int[] array, TaskType taskType, int T)
+        {
+            if (i >= j) return (false, res);
+            else
+            {
+                switch (taskType)
+                {
+                    case TaskType.Sum:
+                        res = IncreaseSum(res, i, array);
+                        break;
+                    case TaskType.Count:
+                        res = IncreaseCount(res, i, array, T);
+                        break;
+                    case TaskType.Max:
+                        res = ChangeMax(res, i, array);
+                        break;
+                }
+                return (true, res);
+            }
+        }
+
+        // Методы для изменения Суммы, Количества элементов или максимального элемента 
+        private static int IncreaseSum(int res, int i, int[] array) => res += array[i];
+        private static int IncreaseCount(int res, int i, int[] array, int T) => res += array[i] > T ? 1 : 0;
+        private static int ChangeMax(int res, int i, int[] array) => Math.Max(array[i], res);
+
+        /// <summary>
+        /// Метод, для расчёта количества элементов > целого T
+        /// </summary>
+        /// <param name="array">Массив целых чисел</param>
+        /// <param name="T">Значение, с которым сравнивается каждый элемент массива</param>
+        /// <returns>Возвращает значения post и количества элементов в массиве > целого T в формате Tuple</returns>
+        public static (bool, int) CountGreaterThanT(int[] array, int T)
+        {
+            bool post = true; // Выполнение постусловия
+            int res = 0; // Результат
+            int j = array.Length; // Длина массива
+            int i = 0;
+
+            // Проверка инварианта на верность перед первой итерацией
+            Debug.Assert(res == 0);
+
+            while (i < j)
+            {
+                // Инвариант перед шагом: res = количество элементов > T среди первых i 
+                res += array[i] > T ? 1 : 0;
+                i++;
+
+                // Инвариант после шага: res = количество элементов > T среди первых i 
+            }
+
+            // Выход i == j => res = количество элементов > T
+            return (post, res);
+        }
+
+        /// <summary>
+        /// Метод, для расчёта максимального элемента в массиве
+        /// </summary>
+        /// <param name="array">Массив целых чисел</param>
+        /// <returns>Возвращает значения post и максимального элемента в массиве в формате Tuple</returns>
+        public static (bool, int) PrefixMax(int[] array)
+        {
+            bool post = true; // Выполнение постусловия
+            int res = 0; // Результат
+            int j = array.Length; // Длина массива
+            int i = 0;
+
+            // Проверка инварианта на верность перед первой итерацией
+            Debug.Assert(res == 0);
+
+            while (i < j)
+            {
+                // Инвариант перед шагом: res = максимальный среди первых i элементов
+                res = Math.Max(array[i], res);
+                i++;
+
+                // Инвариант после шага: res = максимальный среди первых i элементов
+            }
+
+            // Выход i == j => res = максимальный элемент массива
+            return (post, res);
+        }
+    }
+}
--- WpfLab3/WpfLab3/MainWindow.xaml.cs
+++ WpfLab3/WpfLab3/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClassLab3;
 
 namespace WpfLab3
 {
@@ -16,9 +17,100 @@ namespace WpfLab3
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int arraySize;
+        public int[] actualArray;
+        public Class1.TaskType checkedTaskType = Class1.TaskType.None;
+        public Class1.Cycle ActualCycle;
+        public int actualT;
+        public bool CycleStarted = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            SliderSizeArray.ValueChanged += SliderSizeArray_Changed;
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonGenerateArray_Click(object sender, RoutedEventArgs e)
+        {
+            int[] array = Class1.GenerateNSizeArray(arraySize, 0, 100);
+            TextBoxArrayElems.Text = string.Join(", ", array);
+        }
+
+        private void SliderSizeArray_Changed(object sender, RoutedEventArgs e) 
+        {
+            LabelSizeArray.Content = (int) SliderSizeArray.Value;
+            arraySize = (int) SliderSizeArray.Value;
+        }
+
+        private void ParseArrayAndT()
+        {
+            actualArray = TextBoxArrayElems.Text.Trim().Split(", ").Select(x => int.Parse(x)).ToArray();
+            actualT = checkedTaskType == Class1.TaskType.Count ? int.Parse(TextBoxT.Text.Trim()) : 0; // Добавить провеки
+            //MessageBox.Show($"{actualT} > {actualArray[0]} = {actualT > actualArray[0]}");
+        }
+
+        private void ButtonMakeStep_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CycleStarted)
+            {
+                ParseArrayAndT();
+                if (checkedTaskType == Class1.TaskType.None)
+                {
+                    MessageBox.Show("Выберите тип операции с помощью RadioButton");
+                    return;
+                }
+                ActualCycle = Class1.InitializeCycle(actualArray, checkedTaskType, actualT);
+                CycleStarted = true;
+            }
+            int? i, j, res;
+            (i, j, res) = Class1.MakeStep(ActualCycle.i, ActualCycle.res, ActualCycle.array ,ActualCycle.taskType, ActualCycle.T);
+            if (i == null && j == null && res == null)
+            {
+                MessageBox.Show($"Цикл закончен! \n Значение res: {ActualCycle.res}");
+                return;
+            }
+            else
+            {
+                ActualCycle.i++;
+                ActualCycle.res = (int)res;
+                TextBoxArrayChanges.Text = $"Изменяемая ячейка: {ActualCycle.array[(int) i]}\n" +
+                    $"Индекс j: {j}\n" +
+                    $"Значение res: {ActualCycle.res}";
+            }
+        }
+
+        public void ChangeStackPanelVisibilty(bool visibilty)
+        {
+            StackPanelTInfo.Visibility = visibilty ? Visibility.Visible : Visibility.Hidden; 
+        }
+
+        private void RadioButtonSum_Checked(object sender, RoutedEventArgs e)
+        {
+            ChangeStackPanelVisibilty(false);
+            checkedTaskType = Class1.TaskType.Sum;
+        }
+        private void RadioButtonMax_Checked(object sender, RoutedEventArgs e)
+        {
+            ChangeStackPanelVisibilty(false);
+            checkedTaskType = Class1.TaskType.Max;
+        }
+        private void RadioButtonCount_Checked(object sender, RoutedEventArgs e)
+        {
+            ChangeStackPanelVisibilty(true);
+            checkedTaskType = Class1.TaskType.Count;
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            ParseArrayAndT();
+            (bool post, int res) = Class1.MakeOperation(actualArray, checkedTaskType, actualT);
+            TextBoxArrayChanges.Text = $"Значение POST: {post}\n" +
+                $"Значение res: {res}";
         }
     }
 }
